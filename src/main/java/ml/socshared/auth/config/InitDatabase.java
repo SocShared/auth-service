12526fc40722948b9c82bf72944ebe1d97f2c39f
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-24 21:23:45 +0300

    Добавил инициализацию сервисов

--- src/main/java/ml/socshared/auth/config/InitDatabase.java
+++ src/main/java/ml/socshared/auth/config/InitDatabase.java
@@ -103,7 +103,69 @@ public class InitDatabase implements InitializingBean {
             socsharedServiceRepository.save(socsharedService);
             log.info("HIBERNATE init service Service Storage");
         } catch (Exception ignore) {}
-
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://api.socshared.ml");
+            socsharedService.setServiceName("Gateway API Service");
+            socsharedService.setServiceId(UUID.fromString("9e671e7d-976f-40d6-a8c4-67912ae12ede"));
+            socsharedService.setServiceSecret(UUID.fromString("10318fc5-aa65-400a-9ed4-0f4ed2e46e09"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service Gateway API Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://vk.socshared.ml");
+            socsharedService.setServiceName("VK Service");
+            socsharedService.setServiceId(UUID.fromString("cb43eee3-3468-4cc2-b6ed-63419e8726ce"));
+            socsharedService.setServiceSecret(UUID.fromString("f769cb1c-bf08-478d-8218-0bb347369dd7"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service VK Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://bstat.socshared.ml");
+            socsharedService.setServiceName("BSTAT Service");
+            socsharedService.setServiceId(UUID.fromString("e7ee788d-c59e-4a96-bdaf-52d6b33df1f3"));
+            socsharedService.setServiceSecret(UUID.fromString("b8500899-b1a1-4b99-984f-08aed46d1aea"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service BSTAT Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://ms.socshared.ml");
+            socsharedService.setServiceName("Mail Sender Service");
+            socsharedService.setServiceId(UUID.fromString("68c5c6d9-fb18-4adb-800e-faac3ac745b9"));
+            socsharedService.setServiceSecret(UUID.fromString("a981045d-e269-4b28-b7b7-af4a885b9dc4"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service Mail Sender Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://text.socshared.ml");
+            socsharedService.setServiceName("Text Analyzer Service");
+            socsharedService.setServiceId(UUID.fromString("58aeed0d-d092-455b-a1a6-dccfea5b89c6"));
+            socsharedService.setServiceSecret(UUID.fromString("98650932-32df-495a-afeb-9c08bdccd200"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service Text Analyzer Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://support.socshared.ml");
+            socsharedService.setServiceName("Support Service");
+            socsharedService.setServiceId(UUID.fromString("31a2ee92-0e6c-45b7-b6cb-810eec2f1041"));
+            socsharedService.setServiceSecret(UUID.fromString("48733b84-9434-4893-9091-cb855c586ca2"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service Support Service");
+        } catch (Exception ignore) {}
+        try {
+            SocsharedService socsharedService = new SocsharedService();
+            socsharedService.setHostUrl("https://stat.socshared.ml");
+            socsharedService.setServiceName("System Statistic Service");
+            socsharedService.setServiceId(UUID.fromString("eeb4585c-1d8f-463c-b441-e5dbb27ec94d"));
+            socsharedService.setServiceSecret(UUID.fromString("fcf25e23-fe55-4df7-b8f1-e5e56d1277fc"));
+            socsharedServiceRepository.save(socsharedService);
+            log.info("HIBERNATE init service System Statistic Service");
+        } catch (Exception ignore) {}
     }
 
 }
