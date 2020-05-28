3a7b8f62e8d89167d462dd3bdd4146c537f97020
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-28 04:50:40 +0300

    Пофиксил запросы в MailSender

--- src/main/java/ml/socshared/auth/client/MailSenderClient.java
+++ src/main/java/ml/socshared/auth/client/MailSenderClient.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface MailSenderClient {
 
     @GetMapping(value = "/api/v1/private/message", produces = MediaType.APPLICATION_JSON_VALUE)
-    SuccessResponse send(SendMessageRequest request, @RequestHeader("Authorization") String token);
+    SuccessResponse send(@RequestBody SendMessageRequest request, @RequestHeader("Authorization") String token);
 
     @GetMapping(value = "/api/v1/private/message/confirm/mail", produces = MediaType.APPLICATION_JSON_VALUE)
-    SuccessResponse sendMailConfirm(SendMessageMailConfirmRequest request, @RequestHeader("Authorization") String token);
+    SuccessResponse sendMailConfirm(@RequestBody SendMessageMailConfirmRequest request, @RequestHeader("Authorization") String token);
 
 }
