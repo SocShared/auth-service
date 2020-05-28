941580d23d5a977d848ba67fb79cfeef055127e7
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-28 04:52:31 +0300

    Пофиксил запросы

--- src/main/java/ml/socshared/auth/client/MailSenderClient.java
+++ src/main/java/ml/socshared/auth/client/MailSenderClient.java
@@ -6,16 +6,17 @@ import ml.socshared.auth.domain.response.SuccessResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "mail-sender-client", url = "${feign.url.mail:}")
 public interface MailSenderClient {
 
-    @GetMapping(value = "/api/v1/private/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/api/v1/private/message", produces = MediaType.APPLICATION_JSON_VALUE)
     SuccessResponse send(@RequestBody SendMessageRequest request, @RequestHeader("Authorization") String token);
 
-    @GetMapping(value = "/api/v1/private/message/confirm/mail", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/api/v1/private/message/confirm/mail", produces = MediaType.APPLICATION_JSON_VALUE)
     SuccessResponse sendMailConfirm(@RequestBody SendMessageMailConfirmRequest request, @RequestHeader("Authorization") String token);
 
 }
