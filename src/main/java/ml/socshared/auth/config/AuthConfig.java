32a414666c31ccb869136ee58ae907c1fcf1a506
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-16 00:02:08 +0300

    Фикс ошибок

--- src/main/java/ml/socshared/auth/config/AuthConfig.java
+++ src/main/java/ml/socshared/auth/config/AuthConfig.java
@@ -1,6 +1,5 @@
 package ml.socshared.auth.config;
 
-import ml.socshared.auth.config.online.OnlineUsersStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,11 +9,6 @@ public class AuthConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public OnlineUsersStore activeUserStore() {
-        return new OnlineUsersStore();
     }
 
 }
